207d46337a540edba23e7d134d13a7b47eda7357
Oleksii Nyskoroda <s34242@pjwtsk.edu.pl>
2026-03-15 13:18:46 +0100

    Dodano console message

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ class Main {
         }
 
         System.out.println("Hello World");
+        System.out.println("Calculating statistics...");
         System.out.println("Average: " + StatisticsHelper.calculateAverage(values));
     }
 }
