300377806d29db5b1bbef3454c28ba42658df5f3
Oleksii Nyskoroda <s34242@pjwtsk.edu.pl>
2026-03-15 13:36:26 +0100

    feature-conflict: Zmienil final message

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,6 @@ class Main {
         System.out.println("Calculating statistics...");
         System.out.println("Average: " + StatisticsHelper.calculateAverage(values));
         System.out.println("Min: " + StatisticsHelper.calculateMin(values));
-        System.out.println("Statistics processing finished");
+        System.out.println("Statistics processing finished - feature");
     }
 }
