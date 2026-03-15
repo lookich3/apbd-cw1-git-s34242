8a591f36f0b9f4174c628ba5f0030c8cae346562
Oleksii Nyskoroda <s34242@pjwtsk.edu.pl>
2026-03-15 13:08:11 +0100

    main: Dodano walidacje dla pustych danych

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ class Main {
     public static void main(String[] args) {
         int[] values = {2, 4, 6, 8};
 
+        if (values.length == 0) {
+            System.out.println("No data provided");
+            return;
+        }
+
         System.out.println("Hello World");
     }
 }
